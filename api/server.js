c1fd36ed791e1ea8051f383091239971190672db
Mehdi Bennar <Mehdi-been@outlook.com>
2024-04-08 04:47:21 +0000

    Deployment

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js'
+import path from 'path'
 import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js';
@@ -12,20 +13,23 @@ dotenv.config();
 
 const PORT = process.env.PORT || 7000;
 
+const __dirname = path.resolve();
 
-//test route
-app.get("/",(req,res) => {
-    //the root route is localhost:7000
-res.send("Hello worldd!");
-});
+
+
 
 app.use(express.json()); //to parse the incoming requests with json payloads (from req.body)
 app.use(cookieParser()); //to have access to the cookie 
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes)
-
+app.use("/api/users", userRoutes);
+ 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+//running our frontend from the server
+app.get("*",(req,res)=> {
+    res.sendFile(path.join(__dirname, "client","dist","index.html"));
+});
 
 server.listen(PORT,() => {
 
